8a4d2557c0239f5f9cad96ccaf6bcf220366fb2c
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-06-07 21:13:00 +0330

    did some dirty stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,69 @@
 "use client";
 import { useState } from "react";
+import { Cipher } from "crypto";
+
+// All this code by Jesse Jackson: https://jsejcksn.github.io/secrets/
+
+function decode(key: string, cipher: string) {
+  // Primary decode operation
+  var arrKey = key.split("");
+  var arrCipher = cipher.split("");
+  var arrKeyNum = uniEncode(arrKey);
+  var arrCipherNum = uniEncode(arrCipher);
+  var arrKeystream = [];
+  for (var i = 0; i < arrCipherNum.length; i++) {
+    var mult = returnInt(i / arrKeyNum.length);
+    var pos = i % arrKeyNum.length;
+    arrKeystream.push(arrKeyNum[pos] + mult);
+  }
+  var arrTextNum = [];
+  for (var j = 0; j < arrCipherNum.length; j++) {
+    arrTextNum.push((arrCipherNum[j] + 65536 - arrKeystream[j]) % 65536);
+  }
+  var arrText = uniDecode(arrTextNum);
+  var text = arrText.join("");
+  return text;
+}
+
+function encode(key: string, text: string) {
+  // Primary encode operation
+  var arrKey = key.split("");
+  var arrText = text.split("");
+  var arrKeyNum = uniEncode(arrKey);
+  var arrTextNum = uniEncode(arrText);
+  var arrKeystream = [];
+  for (var i = 0; i < arrTextNum.length; i++) {
+    var mult = returnInt(i / arrKeyNum.length);
+    var pos = i % arrKeyNum.length;
+    arrKeystream.push(arrKeyNum[pos] + mult);
+  }
+  var arrCipherNum = [];
+  for (var j = 0; j < arrTextNum.length; j++) {
+    arrCipherNum.push((arrTextNum[j] + arrKeystream[j]) % 65536);
+  }
+  var arrCipher = uniDecode(arrCipherNum);
+  var cipher = arrCipher.join("");
+  return cipher;
+}
+
+function returnInt(n: any) {
+  // Staple function
+  return parseInt(n, 10);
+}
+
+function uniDecode(arr: any[]) {
+  // Maps an array of Unicode values to their UTF-8 characters
+  return arr.map(function (x) {
+    return String.fromCharCode(x);
+  });
+}
+
+function uniEncode(arr: any[]) {
+  // Maps an array of UTF-8 characters to their Unicode values
+  return arr.map(function (x) {
+    return x.charCodeAt(0);
+  });
+}
 
 function getRandomEnglishCharacter(chars: string) {
   const randomIndex = Math.floor(Math.random() * chars.length);
@@ -11,12 +75,13 @@ export default function Home() {
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz            ,./\"'";
   const [value, setValue] = useState("Start typing!!!");
   const [chars, setChars] = useState(defaultChars);
+  const [key, setKey] = useState("gulp");
 
   return (
     <div className="min-h-screen bg-gray-950 p-10 text-white">
       <h1 className="text-3xl mb-10">Random Typer!</h1>
       <textarea
-        className="bg-gray-50 rounded w-96 h-64 text-black p-2 text-lg"
+        className={`bg-gray-50 rounded w-96 h-64 text-black p-2 text-lg`}
         value={value}
         onChange={(e) => {
           if (value.length < e.target.value.length) {
@@ -48,12 +113,17 @@ export default function Home() {
         <button
           className="bg-gray-700 hover:bg-gray-800 transition rounded h-12 mx-1 text-white/80 cursor-pointer p-2"
           onClick={() => {
-            ;
+            setValue(encode(key, value));
           }}
         >
-          Decode the text using the key of
-          <input className="bg-amber-50/50 p-1 rounded text-black" />
+          Encode
         </button>
+        <span> Put the key here: </span>{" "}
+        <input
+          className="bg-amber-50/50 p-1 rounded text-black ms-2"
+          value={key}
+          onChange={(e) => setKey(e.target.value)}
+        />
       </div>
     </div>
   );
