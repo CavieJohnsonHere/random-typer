d5af9646d107b68d0f5420b8ee4d0c5086b20953
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-06-07 21:13:55 +0330

    that was some OLD code!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,43 +6,43 @@ import { Cipher } from "crypto";
 
 function decode(key: string, cipher: string) {
   // Primary decode operation
-  var arrKey = key.split("");
-  var arrCipher = cipher.split("");
-  var arrKeyNum = uniEncode(arrKey);
-  var arrCipherNum = uniEncode(arrCipher);
-  var arrKeystream = [];
-  for (var i = 0; i < arrCipherNum.length; i++) {
-    var mult = returnInt(i / arrKeyNum.length);
-    var pos = i % arrKeyNum.length;
+  let arrKey = key.split("");
+  let arrCipher = cipher.split("");
+  let arrKeyNum = uniEncode(arrKey);
+  let arrCipherNum = uniEncode(arrCipher);
+  let arrKeystream = [];
+  for (let i = 0; i < arrCipherNum.length; i++) {
+    let mult = returnInt(i / arrKeyNum.length);
+    let pos = i % arrKeyNum.length;
     arrKeystream.push(arrKeyNum[pos] + mult);
   }
-  var arrTextNum = [];
-  for (var j = 0; j < arrCipherNum.length; j++) {
+  let arrTextNum = [];
+  for (let j = 0; j < arrCipherNum.length; j++) {
     arrTextNum.push((arrCipherNum[j] + 65536 - arrKeystream[j]) % 65536);
   }
-  var arrText = uniDecode(arrTextNum);
-  var text = arrText.join("");
+  let arrText = uniDecode(arrTextNum);
+  let text = arrText.join("");
   return text;
 }
 
 function encode(key: string, text: string) {
   // Primary encode operation
-  var arrKey = key.split("");
-  var arrText = text.split("");
-  var arrKeyNum = uniEncode(arrKey);
-  var arrTextNum = uniEncode(arrText);
-  var arrKeystream = [];
-  for (var i = 0; i < arrTextNum.length; i++) {
-    var mult = returnInt(i / arrKeyNum.length);
-    var pos = i % arrKeyNum.length;
+  let arrKey = key.split("");
+  let arrText = text.split("");
+  let arrKeyNum = uniEncode(arrKey);
+  let arrTextNum = uniEncode(arrText);
+  let arrKeystream = [];
+  for (let i = 0; i < arrTextNum.length; i++) {
+    let mult = returnInt(i / arrKeyNum.length);
+    let pos = i % arrKeyNum.length;
     arrKeystream.push(arrKeyNum[pos] + mult);
   }
-  var arrCipherNum = [];
-  for (var j = 0; j < arrTextNum.length; j++) {
+  let arrCipherNum = [];
+  for (let j = 0; j < arrTextNum.length; j++) {
     arrCipherNum.push((arrTextNum[j] + arrKeystream[j]) % 65536);
   }
-  var arrCipher = uniDecode(arrCipherNum);
-  var cipher = arrCipher.join("");
+  let arrCipher = uniDecode(arrCipherNum);
+  let cipher = arrCipher.join("");
   return cipher;
 }
 
