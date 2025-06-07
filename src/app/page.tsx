66e9b0f76386a561c9e6e7c7557a6745153aa86c
Mohammad Dargahi <caviejohnsonhere@gmail.com>
2025-06-07 20:45:18 +0330

    added a character change menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,44 @@
 "use client";
 import { useState } from "react";
 
-function getRandomEnglishCharacter() {
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz            ,./\"'";
+function getRandomEnglishCharacter(chars: string) {
   const randomIndex = Math.floor(Math.random() * chars.length);
   return chars[randomIndex];
 }
 
 export default function Home() {
+  const defaultChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz            ,./\"'";
   const [value, setValue] = useState("Start typing!!!");
+  const [chars, setChars] = useState(defaultChars);
 
   return (
     <div className="min-h-screen bg-gray-950 p-10 text-white">
       <h1 className="text-3xl mb-10">Random Typer!</h1>
-
       <textarea
         className="bg-gray-50 rounded w-96 h-64 text-black p-2 text-lg"
         value={value}
         onChange={(e) => {
           if (value.length < e.target.value.length) {
-            setValue(value + getRandomEnglishCharacter());
+            setValue(value + getRandomEnglishCharacter(chars));
           } else {
             setValue(e.target.value);
           }
         }}
       />
+      <div className="mt-5">
+        <span>
+          You can change the character string here. Characters apear more
+          frequently if you put more of them here:{" "}
+        </span>
+        <input
+          type="text"
+          className="bg-gray-50 text-black p-2 rounded h-12 mx-1"
+          value={chars}
+          onChange={(e) => setChars(e.target.value)}
+        />
+        <button className="bg-gray-900 rounded h-12 mx-1 text-white/80 cursor-pointer p-2" onClick={() => setChars(defaultChars)}>Reset</button>
+      </div>
     </div>
   );
 }
